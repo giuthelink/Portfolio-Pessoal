193f31290b7904095268de57a448ab385de17a39
Giuliano Ferreira <81049796+giuthelink@users.noreply.github.com>
2025-10-17 09:28:58 -0300

    Portfólio com projetos e serviços que realizo

---
+++ script.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120; // compensação da navbar
+    if (window.scrollY >= sectionTop) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
